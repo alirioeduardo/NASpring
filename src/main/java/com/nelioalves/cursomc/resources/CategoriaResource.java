d3434efde1a0389072074b403e031d7ecf4ca5b3
Eduardo Pires <edu.aps@hotmail.com>
2021-05-12 21:02:26 -0300

    Second commit

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.nelioalves.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	@GetMapping
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 	
